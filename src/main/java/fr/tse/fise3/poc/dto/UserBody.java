102ad6b56a131cf25f60fdd076662f986ab3b6cc
Taha Alami Idrissi <3624559-TahaAlamiIdrissi@users.noreply.gitlab.com>
2021-02-03 20:10:59 +0100

    feat: #17 created a dto to handle the upcomming request

---
+++ src/main/java/fr/tse/fise3/poc/dto/UserBody.java
@@ -0,0 +1,14 @@
+package fr.tse.fise3.poc.dto;
+
+import java.time.Instant;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserBody {
+	private Long userId;
+}
