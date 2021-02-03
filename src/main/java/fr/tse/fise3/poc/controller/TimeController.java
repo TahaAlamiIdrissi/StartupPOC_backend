b48b808093b6b65b5e4575679434203d3a9537f0
mariam9498 <mm.bouhriz@gmail.com>
2021-02-03 19:25:22 +0100

    Time controller class

--- src/main/java/fr/tse/fise3/poc/controller/TimeController.java
+++ src/main/java/fr/tse/fise3/poc/controller/TimeController.java
@@ -1,10 +1,12 @@
 package fr.tse.fise3.poc.controller;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,4 +36,8 @@ public class TimeController {
 		return this.timeService.createTime(timeRequest) ;
 	}
 
+	@PostMapping("/time/user/{userId}")
+	Collection<Time> findUserTimesForManager(@PathVariable Long userId) {
+		return this.timeService.findTimesOfUser(userId) ;
+	}
 }
