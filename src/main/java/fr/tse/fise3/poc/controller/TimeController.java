3127eddcd92bd9630ab0e4db694d7f7c79b00d30
mariam9498 <mm.bouhriz@gmail.com>
2021-02-05 16:50:48 +0100

    Feat: #4 manager can see his employees times

--- src/main/java/fr/tse/fise3/poc/controller/TimeController.java
+++ src/main/java/fr/tse/fise3/poc/controller/TimeController.java
@@ -1,7 +1,6 @@
 package fr.tse.fise3.poc.controller;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
