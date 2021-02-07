e0dae1b93a791ce8ee89b36e9467d61a22f6eb46
Asmae <asmae.nedday1998@gmail.com>
2021-02-07 13:52:52 +0100

    fixing name

--- src/main/java/fr/tse/fise3/poc/service/MailContentBuilderService.java
+++ src/main/java/fr/tse/fise3/poc/service/MailContentBuilderService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-public interface MailContentBuilder {
+public interface MailContentBuilderService {
 	
 	public String build(String message);
 	
--- src/main/java/fr/tse/fise3/poc/service/impl/MailContentBuilderServiceImpl.java
+++ src/main/java/fr/tse/fise3/poc/service/impl/MailContentBuilderServiceImpl.java
@@ -3,12 +3,12 @@ package fr.tse.fise3.poc.service.impl;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-import fr.tse.fise3.poc.service.MailContentBuilder;
+import fr.tse.fise3.poc.service.MailContentBuilderService;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class MailContentBuilderImpl implements MailContentBuilder {
+public class MailContentBuilderServiceImpl implements MailContentBuilderService {
 	private final TemplateEngine templateEngine;
 
 	public String build(String message) {
